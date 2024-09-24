bd1672bcf9a8e04813fe2ff340e0526da4573222
sheena849 <sheenamugo04@gmail.com>
2024-09-24 15:55:23 +0300

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let a = 2000
+    let b = 100
+    return a/b
+}
+function square(){
+    let x = 2
+    return x*2
+}
+function add (){
+    let a = 3
+    let b = 4
+    return a+b
+}
